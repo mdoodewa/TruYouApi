da622d704fca701e2171f4c97d69fb60ee459599
Martijn <mdoodewa@avans.nl>
2021-06-21 14:31:43 +0200

    added verify

--- api/controllers/TruYouController.js
+++ api/controllers/TruYouController.js
@@ -3,12 +3,41 @@
 module.exports = {
 
     update_stream_data: (req, res, next) => {
-        res.status(200).json({'nice': 'nice'});
-        console.log(req.body);
+        const cert = fs.readFileSync(path.join(__dirname,
+            'public.der'), 'utf8');
+        
+        let certStore = new Map([["My Name", cert]]);
+
+        if (certStore.has(req.body.data.name)) {
+            let certificate = certStore.get(req.body.data.name);
+            let publicKey = crypto.createPublicKey(certificate).export({type:'spki', format:'pem'});
+    
+            console.log(verifySignature(publicKey, req.body.signature, JSON.stringify(req.body.data)));
+    
+            res.writeHead(200);
+        }
+        else {
+            res.writeHead(400);
+        }
+        res.end();
+    
     },
 
     create_stream_data: (req, res, next) =>{
         res.status(200).json({'nice' : 'ook zeer nice'});
         console.log(req.body);
     }
+}
+
+function verifySignature(key, signature, data) {
+    console.log(signature);
+    const verifier = crypto.createVerify('RSA-SHA256');
+    
+    verifier.update(data, 'ascii');
+
+    const publicKeyBuf = Buffer.from(key, 'ascii');
+    const signatureBuf = Buffer.from(signature, 'hex');
+    const result = verifier.verify(publicKeyBuf, signatureBuf);
+
+    return result;
 }
