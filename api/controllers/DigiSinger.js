6f8d07afe42990ae30ef010fcea94b32c5fc60fb
Martijn <mdoodewa@avans.nl>
2021-06-21 15:21:20 +0200

    -

---
+++ api/controllers/DigiSinger.js
@@ -0,0 +1,32 @@
+const crypto = require('crypto');
+
+// Class used for signing and encoding of data
+class DigiSigner {
+    constructor(algorithm = 'RSA-SHA256', securityEncoding = 'hex') {
+        this.algorithm = algorithm;
+        this.securityEncoding = securityEncoding;
+    }
+
+    // Generates a digital signature based on provided data and private key
+    // Input should be either string or buffer
+    signData(rawData, privKey) {
+        let signer = crypto.createSign(this.algorithm);
+
+        signer.write(rawData);
+        signer.end();
+
+        return signer.sign(privKey, this.securityEncoding);
+    }
+
+    // Validates if data is consistent with given signature and public key
+    // Input should be either string or buffer
+    verifySignature(publicKey, signature, rawData) {
+        let verifier = crypto.createVerify(this.algorithm);
+        
+        verifier.update(rawData);
+
+        return verifier.verify(publicKey, signature, this.securityEncoding);
+    }
+}
+
+module.exports = DigiSigner;
--- api/controllers/TruYouController.js
+++ api/controllers/TruYouController.js
@@ -2,21 +2,33 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 
+import DigiSigner from './DigiSinger';
+const signer = new DigiSigner();
+
+const publicKey = fs.readFileSync(path.join(__dirname,
+    'public.der'), 'utf8');
+const privateKey = fs.readFileSync(path.join(__dirname,
+    'private.der'), 'utf8');
+
+
+
 module.exports = {
 
     update_stream_data: (req, res, next) => {
-        const cert = fs.readFileSync(path.join(__dirname,
-            'public.der'), 'utf8');
         
         let certStore = new Map([["My Name", cert]]);
 
         if (certStore.has("My Name")) {
             let certificate = certStore.get("My Name");
-            let publicKey = crypto.createPublicKey(certificate).export({type:'spki', format:'der'});
-    
-            console.log(verifySignature(publicKey, req.body.signature, JSON.stringify(req.body.data)));
+            // let publicKey = crypto.createPublicKey(certificate).export({type:'spki', format:'pem'});
+            let valueVerify = signer.verifySignature(publicKey, privateKey, actualData);
+
+            console.log(valueVerify);
+
+            console.log(signer.signData());
     
             res.writeHead(200);
+    
         }
         else {
             res.writeHead(400);
@@ -29,17 +41,4 @@ module.exports = {
         res.status(200).json({'nice' : 'ook zeer nice'});
         console.log(req.body);
     }
-}
-
-function verifySignature(key, signature, data) {
-    console.log(signature);
-    const verifier = crypto.createVerify('RSA-SHA256');
-    
-    verifier.update(data, 'ascii');
-
-    const publicKeyBuf = Buffer.from(key, 'ascii');
-    const signatureBuf = Buffer.from(signature, 'hex');
-    const result = verifier.verify(publicKeyBuf, signatureBuf);
-
-    return result;
 }
